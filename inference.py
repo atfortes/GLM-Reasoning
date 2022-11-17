d84359c1a902c0ed1162580f8c7d3fa872546bee
atfortes <atfortes@protonmail.com>
2022-11-17 01:31:29 +0000

    Add separate main for manual experiments

--- inference.py
+++ inference.py
@@ -1,49 +1,27 @@
-import requests
-import json
-from datasets import GSM8K
-import re
 import yaml
+import json
+import requests
+from datasets import *
 
-with open('config.yml', 'r') as file:
+with open("config.yml", 'r') as file:
     config = yaml.safe_load(file)
 
-gsm8k = GSM8K(config)
 
-# Construct prompt
-prompt = "Answer the following questions. Show your work and write your answer after '####'. \n"
-prompt += "".join(["Question: " + example['question'] + "\n" +
-                  "Answer: " + example['answer'] + "\n" for example in gsm8k.train[:5]])
-
-
-def sample(question, seed):
-
-    full_prompt = prompt + "Question: " + question + \
-        " Answer: Let's think step by step. "
-    
-    # Remove calculator tokens from dataset
-    full_prompt = re.sub(r'{}.*?{}'.format(re.escape("<<"),
-                                           re.escape(">>")), '', full_prompt)
-
-    headers = {'Content-Type': 'application/json'}
-    data = {
-        "apikey": config['api_key'],
-        "apisecret": config['api_secret'],
-        "language": "en-US",
-        "prompt": full_prompt,
-        "length_penalty": 0.7,
-        "sampling_strategy": "BaseStrategy",
-        "temperature": 0.5,
-        "top_k": 40,
-        "top_p": 0,
-        "min_gen_length": 10,
-        "max_tokens": 512,
-        "seed": seed
-    }
-
+def sample(prompt, length_penalty=0.7, temperature=0.9, max_tokens=200, stop=[], seed=1234):
     response = requests.post(
-        config['request_url'], headers=headers, data=json.dumps(data))
+        config['request_url'],
+        headers={"Content-Type": "application/json"},
+        data=json.dumps({
+            "apikey": config["api_key"],
+            "apisecret": config["api_secret"],
+            "language": "en-US",
+            "prompt": prompt,
+            "length_penalty": length_penalty,
+            "sampling_strategy": "BaseStrategy",
+            "temperature": temperature,
+            "max_tokens": max_tokens,
+            "stop": stop,
+            "seed": seed
+        })
+    )
     return response.json()['result']['output']['text']
-
-
-for i in range(1, 5):
-    print(sample(gsm8k.train[5]['question'], seed=i))
---
+++ main.py
@@ -0,0 +1,43 @@
+import yaml
+import json
+import datasets
+import argparse
+import numpy as np
+from inference import sample
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-ds', '--dataset', type=str, default="GSM8K", choices=["GSM8K", "CommonsenseQA", "StrategyQA"])
+    parser.add_argument('-sn', '--samples_num', type=int, default=10)
+    parser.add_argument('-rs', '--random_samples', type=bool, default=False)
+    parser.add_argument('-lp', '--length_penalty', type=float, default=0.7)
+    parser.add_argument('-t', '--temperature', type=float, default=0.1)
+    parser.add_argument('-mt', '--max_tokens', type=int, default=128)
+    parser.add_argument('-st', '--stop_token', type=list, default=["\n\n"])
+    parser.add_argument('-s', '--seed', type=int, default=1234)
+    args = parser.parse_args()
+
+    with open("config.yml", 'r') as file:
+        config = yaml.safe_load(file)
+
+    print('Loading dataset...')
+    dataset = getattr(datasets, args.dataset)(config)
+    few_shot_cot_exemplars = "\n\n".join(json.load(open("few-shot-exemplars/few-shot-cot-exemplars.json", 'r'))[args.dataset])
+
+    print('Sampling...')
+    samples_num = min(len(dataset.questions), args.samples_num)
+    for i in np.random.choice(range(len(dataset.questions)), size=samples_num) if args.random_samples else range(samples_num):
+        question = dataset.questions[i]
+        prompt = f'''{few_shot_cot_exemplars}\n\nQ: {question}\nA:'''
+        print("="*34, "Question", i, "="*34)
+        print(question)
+        print("Correct Answer:", dataset.answers[i])
+        print("Predicted Answer:", sample(
+            prompt,
+            args.length_penalty,
+            args.temperature,
+            args.max_tokens,
+            args.stop_token,
+            args.seed
+        ))
