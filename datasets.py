d7b7a331ab47030e505495aeccd0a1f2f6bc1b6b
atfortes <atfortes@protonmail.com>
2022-11-29 11:38:03 -0500

    Add answer extraction regex for datasets

--- datasets.py
+++ datasets.py
@@ -1,29 +1,11 @@
 import json
 import re
 
-ANS_RE = re.compile(r"#### (\-?[0-9\.\,]+)")
-INVALID_ANS = "[invalid]"
 
-
-def extract_answer(completion):
-    match = ANS_RE.search(completion)
-    if match:
-        match_str = match.group(1).strip()
-        match_str = match_str.replace(",", "")
-        return match_str
-    else:
-        return INVALID_ANS
-
-
-def is_correct(model_completion, gt_example):
-    gt_answer = extract_answer(gt_example["answer"])
-    assert gt_answer != INVALID_ANS
-    return extract_answer(model_completion) == gt_answer
-
-
-class GSM8K():
+class GSM8K:
     # math word problems
-    ANS_RE = re.compile(r"#### (\-?[0-9\.\,]+)")
+
+    ANS_RE = re.compile(r"(\-?[0-9\.\,]+)")
 
     def __init__(self, config, file_name="test.jsonl"):
         with open(config["GSM8K_path"] + file_name) as f:
@@ -33,29 +15,35 @@ class GSM8K():
         assert len(self.questions) == len(self.answers)
 
     def extract_answer(self, completion):
-        match = self.ANS_RE.search(completion)
+        match = re.compile(r"#### (\-?[0-9\.\,]+)").search(completion)
         match_str = match.group(1).strip()
         return match_str
 
 
-class CommonsenseQA():
+class CommonsenseQA:
     # multiple choice questions
+
+    ANS_RE = re.compile(r"\(a\)|\(b\)|\(c\)|\(d\)\|\(e\)")
+
     def __init__(self, config, file_name="dev_rand_split.jsonl"):
         with open(config["CommonsenseQA_path"] + file_name) as f:
             samples = list(map(json.loads, f.readlines()))
             self.questions = list(map(
-                lambda s: "Q: " + s["question"]["stem"] + "\nAnswer Choices:\n" + \
+                lambda s: s["question"]["stem"] + "\nAnswer Choices:\n" + \
                     "\n".join(map(lambda c: f'''({c["label"].lower()}) {c["text"]}''', s["question"]["choices"])), samples
             ))
             self.answers = list(map(lambda s: f'''({s["answerKey"].lower()})''', samples))
         assert len(self.questions) == len(self.answers)
 
 
-class StrategyQA():
+class StrategyQA:
     # yes or no questions
+
+    ANS_RE = re.compile(r"yes|no")
+
     def __init__(self, config, file_name="strategyqa_train.json"):
         with open(config["StrategyQA_path"] + file_name, encoding='utf-8') as f:
             samples = json.load(f)
-            self.questions = list(map(lambda s: "Q: " + s["question"], samples))
+            self.questions = list(map(lambda s: s["question"], samples))
             self.answers = list(map(lambda s: "yes" if s["answer"] else "no", samples))
         assert len(self.questions) == len(self.answers)
