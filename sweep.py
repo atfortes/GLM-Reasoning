2926b1cf69a2c4b23a05ea47729a316c2e199587
atfortes <atfortes@protonmail.com>
2022-12-08 17:15:58 +0000

    add abstraction to different datasets and types of prompts

--- sweep.py
+++ sweep.py
@@ -1,126 +1,121 @@
+import re
+import sys
+import yaml
+import json
 import wandb
 import argparse
-import json
-import yaml
 import datasets
-import re
 import traceback
-import sys
-
-import numpy as np
 from inference import sample
 
 
 def evaluate():
-    run = wandb.init()
+    wandb.init()
 
     total_correct = 0
-    total_correct_members = 0
+    total_correct_voters = 0
     log_data = []
 
     with open("config.yml", 'r') as file:
         config = yaml.safe_load(file)
 
-    if wandb.config.language == 'en-US':
-        few_shot_cot_exemplars = "\n\n".join(json.load(open(
-            "few-shot-exemplars/few-shot-cot-exemplars.json", 'r'))[wandb.config.dataset])
-        file_name = "train.jsonl"
-        question_marker = "Q"
-        answer_marker = "A"
+    dataset = getattr(datasets, wandb.config.dataset)(config, "dev_rand_split.jsonl")
 
-        answer_prefix = "The answer is"
+    if wandb.config.type == 'z':
+        prefix = ""
+        answer_prefix = dataset.ZERO_SHOT_PROMPT
+    elif wandb.config.type == 'f':
+        prefix = "\n\n".join(json.load(open(
+                 "few-shot-exemplars/few-shot-exemplars.json", 'r'))[wandb.config.dataset]) + "\n\n"
+        answer_prefix = ""
+    elif wandb.config.type == 'f-cot':
+        prefix = "\n\n".join(json.load(open(
+                 "few-shot-exemplars/few-shot-cot-exemplars.json", 'r'))[wandb.config.dataset]) + "\n\n"
+        answer_prefix = ""
 
-    elif wandb.config.language == 'zh-CN':
-        few_shot_cot_exemplars = "\n\n".join(json.load(open(
-            "few-shot-exemplars/few-shot-cot-exemplars-cn.json", 'r'))[wandb.config.dataset])
-        file_name = "train_cn.jsonl"
-        question_marker = "问"
-        answer_marker = "答"
-
-        answer_prefix = "答案是"
-
-    else:
-        raise ValueError("language needs to be en-US or zh-CN!")
-
-    dataset = getattr(datasets, wandb.config.dataset)(config, file_name)
+    answer_partition = "answer is"
 
     samples_num = min(len(dataset.questions), wandb.config.samples_num)
-    for i in np.random.choice(range(len(dataset.questions)), size=samples_num) if wandb.config.random_samples else range(samples_num):
-        question = dataset.questions[i]
-        prompt = f'''{few_shot_cot_exemplars}\n\n{question_marker}: {question}\n{answer_marker}:'''
+    for i in range(1, samples_num + 1):
+        try:
+            question = dataset.questions[i]
 
-        target = dataset.answers[i]
-        predictions_full = [sample(
-            prompt,
-            wandb.config.length_penalty,
-            wandb.config.temperature,
-            wandb.config.max_tokens,
-            wandb.config.stop_token,
-            seed
-        )[0] for seed in range(1, wandb.config.voting_num)]
+            prompt = f'''{prefix}Q: {question}\nA: {answer_prefix}'''
 
-        predictions = [re.findall(
-            r'\d+', p.partition(answer_prefix)[2]) for p in predictions_full]
-        predictions = [p[0] if len(p) > 0 else "" for p in predictions]
+            target = dataset.answers[i]
+            predictions_full = [sample(
+                prompt,
+                wandb.config.length_penalty,
+                wandb.config.temperature,
+                wandb.config.max_tokens,
+                wandb.config.stop_token,
+                seed
+            )[0] for seed in range(1, wandb.config.voting_num + 1)]
 
-        log_data.extend([[question, p_full, p, target]
-                        for p_full, p in zip(predictions_full, predictions)])
+            predictions = [re.findall(dataset.ANS_RE, p if wandb.config.type == 'z' else p.partition(answer_partition)[2])
+                        for p in predictions_full]
+            predictions = [p[0] if len(p) > 0 else "[NO ANSWER]" for p in predictions]
+            log_data.extend([[question, p_full, p, target]
+                            for p_full, p in zip(predictions_full, predictions)])
+            score = sum([int(p == target) for p in predictions]) / wandb.config.voting_num
+            total_correct_voters += sum([int(p == target) for p in predictions])
+            final_prediction = max(set(predictions), key=predictions.count)
+            correct = final_prediction == target
+            total_correct += correct
 
-        score = sum([int(p == target)
-                    for p in predictions]) / wandb.config.voting_num
-        total_correct_members += sum([int(p == target) for p in predictions])
-        final_prediction = max(set(predictions), key=predictions.count)
-        correct = int(final_prediction == target)
-        total_correct += correct
+            wandb.log({
+                'correct': int(correct),
+                'correct ratio': score,
+                'accuracy': total_correct / i
+            })
+        except KeyboardInterrupt:
+            break
+        except Exception as e:
+            print(f"error in {i}: {e}")
+            break
 
-        wandb.log({
-            'correct ratio': score,
-            'correct': correct
-        })
-
-    columns = ["question",
-               "prediction (full)", "prediction (extracted)", "target"]
     wandb.log({
-        'val_acc': total_correct / samples_num,
-        'val_correct_ratio': total_correct_members / (samples_num * wandb.config.voting_num),
-        'predictions': wandb.Table(data=log_data, columns=columns),
+        'total accuracy': total_correct / samples_num,
+        'total correct ratio': total_correct_voters / (samples_num * wandb.config.voting_num),
+        'predictions': wandb.Table(
+            data=log_data,
+            columns=["question", "prediction (full)", "prediction (extracted)", "target"]),
     })
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-ds', '--dataset', type=str, default="GSM8K",
+    parser.add_argument('-ds', '--dataset', type=str, default="CommonsenseQA",
                         choices=["GSM8K", "CommonsenseQA", "StrategyQA"])
-    parser.add_argument('-sn', '--samples_num', type=int, default=10)
-    parser.add_argument('-rs', '--random_samples', type=bool, default=False)
-    parser.add_argument('-lp', '--length_penalty', type=float, default=0.7)
-    parser.add_argument('-t', '--temperature', type=float, default=0.1)
-    parser.add_argument('-mt', '--max_tokens', type=int, default=128)
-    parser.add_argument('-st', '--stop_token', type=list, default=["\n\n"])
+    parser.add_argument('-t', '--type', type=str, default="f",
+                        choices=["z", "f", "f-cot"])
+    parser.add_argument('-sn', '--samples_num', type=int, default=2000)
     parser.add_argument('-vn', '--voting_num', type=int, default=1)
+    parser.add_argument('-lp', '--length_penalty', type=float, default=0.99)
+    parser.add_argument('-tmp', '--temperature', type=float, default=0.01)
+    parser.add_argument('-mt', '--max_tokens', type=int, default=10)
+    parser.add_argument('-st', '--stop_token', type=list, default=["Q:"])
     args = parser.parse_args()
 
     sweep_configuration = {
-        'method': 'bayes',
-        'name': 'GSM8K_parameter_sweep',
-        'metric': {'goal': 'maximize', 'name': 'correct ratio'},
+        'method': 'random',
+        'name': 'CommonsenseQA (zero-shot)',
+        'metric': {'goal': 'maximize', 'name': 'accuracy'},
         'parameters':
         {
             'dataset': {'value': args.dataset},
+            'type': {'value': args.type},
             'samples_num': {'value': args.samples_num},
-            'random_samples': {'value': args.random_samples},
             'max_tokens': {'value': args.max_tokens},
             'stop_token': {'value': args.stop_token},
             'voting_num': {'value': args.voting_num},
-
-            'language': {'values': ['en-US', 'zh-CN']},
-            'length_penalty': {'max': 0.99, 'min': 0.01},
-            'temperature': {'max': 0.99, 'min': 0.01}
+            'length_penalty': {'value': args.length_penalty},
+            'temperature': {'value': args.temperature}
         }
     }
 
     sweep_id = wandb.sweep(sweep=sweep_configuration, project='GLM-Reasoning')
-    wandb.agent(sweep_id, function=evaluate, count=30)
+    wandb.agent(sweep_id, function=evaluate, count=1)
 
 
 if __name__ == '__main__':
