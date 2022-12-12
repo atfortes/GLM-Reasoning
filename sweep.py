df167adb82cf941fdcb6132e3498db37dff73856
atfortes <atfortes@protonmail.com>
2022-12-12 15:18:54 +0000

    added zero-shot cot and better answer extraction for csqa

--- sweep.py
+++ sweep.py
@@ -9,37 +9,51 @@ import traceback
 from inference import sample
 
 
+def extract_answer(question, prediction, ans_re):
+    answer = re.findall(ans_re, prediction.lower().partition("the answer is ")[2] if wandb.config.type == 'f-cot' else prediction)
+    if len(answer) > 0:
+        return answer[0]
+    elif wandb.config.dataset in ("CommonsenseQA",):
+        part = question.partition(prediction)
+        if part[1] != "":
+            answer = re.findall(ans_re, part[0].split("\n")[-1])
+        if len(answer) > 0:
+            return answer[0]
+    return "[NO ANSWER]"
+
+
 def evaluate():
     wandb.init()
-
     total_correct = 0
     total_correct_voters = 0
-    log_data = []
+    all_predictions_log_data = []
 
     with open("config.yml", 'r') as file:
         config = yaml.safe_load(file)
 
-    dataset = getattr(datasets, wandb.config.dataset)(config, "dev_rand_split.jsonl")
+    dataset = getattr(datasets, wandb.config.dataset)(config, "test.jsonl")
 
     if wandb.config.type == 'z':
         prefix = ""
         answer_prefix = dataset.ZERO_SHOT_PROMPT
+    if wandb.config.type == 'z-cot':
+        prefix = ""
+        answer_prefix = "Let's think step by step."
     elif wandb.config.type == 'f':
         prefix = "\n\n".join(json.load(open(
                  "few-shot-exemplars/few-shot-exemplars.json", 'r'))[wandb.config.dataset]) + "\n\n"
-        answer_prefix = ""
+        answer_prefix = "The answer is [MASK]."
     elif wandb.config.type == 'f-cot':
         prefix = "\n\n".join(json.load(open(
                  "few-shot-exemplars/few-shot-cot-exemplars.json", 'r'))[wandb.config.dataset]) + "\n\n"
         answer_prefix = ""
 
-    answer_partition = "answer is"
-
     samples_num = min(len(dataset.questions), wandb.config.samples_num)
     for i in range(1, samples_num + 1):
         try:
-            question = dataset.questions[i]
+            predictions_log_data = []
 
+            question = dataset.questions[i]
             prompt = f'''{prefix}Q: {question}\nA: {answer_prefix}'''
 
             target = dataset.answers[i]
@@ -52,11 +66,19 @@ def evaluate():
                 seed
             )[0] for seed in range(1, wandb.config.voting_num + 1)]
 
-            predictions = [re.findall(dataset.ANS_RE, p if wandb.config.type == 'z' else p.partition(answer_partition)[2])
-                        for p in predictions_full]
-            predictions = [p[0] if len(p) > 0 else "[NO ANSWER]" for p in predictions]
-            log_data.extend([[question, p_full, p, target]
-                            for p_full, p in zip(predictions_full, predictions)])
+            if wandb.config.type == 'z-cot':
+                predictions_full = [sample(
+                    f"{prompt} {chain_of_thought} The answer is [MASK].",
+                    wandb.config.length_penalty,
+                    wandb.config.temperature,
+                    10
+                )[0] for chain_of_thought in predictions_full]
+
+            predictions = [extract_answer(question, p, dataset.ANS_RE)
+                           for p in predictions_full]
+            predictions_log_data = [[question, p_full, p, target]
+                                    for p_full, p in zip(predictions_full, predictions)]
+            all_predictions_log_data.extend(predictions_log_data)
             score = sum([int(p == target) for p in predictions]) / wandb.config.voting_num
             total_correct_voters += sum([int(p == target) for p in predictions])
             final_prediction = max(set(predictions), key=predictions.count)
@@ -66,7 +88,10 @@ def evaluate():
             wandb.log({
                 'correct': int(correct),
                 'correct ratio': score,
-                'accuracy': total_correct / i
+                'accuracy': total_correct / i,
+                'predictions': wandb.Table(
+                    data=predictions_log_data,
+                    columns=["question", "prediction (full)", "prediction (extracted)", "target"])
             })
         except KeyboardInterrupt:
             break
@@ -77,29 +102,30 @@ def evaluate():
     wandb.log({
         'total accuracy': total_correct / samples_num,
         'total correct ratio': total_correct_voters / (samples_num * wandb.config.voting_num),
-        'predictions': wandb.Table(
-            data=log_data,
-            columns=["question", "prediction (full)", "prediction (extracted)", "target"]),
+        'all_predictions': wandb.Table(
+            data=all_predictions_log_data,
+            columns=["question", "prediction (full)", "prediction (extracted)", "target"]
+        )
     })
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-ds', '--dataset', type=str, default="CommonsenseQA",
+    parser.add_argument('-ds', '--dataset', type=str, default="GSM8K",
                         choices=["GSM8K", "CommonsenseQA", "StrategyQA"])
-    parser.add_argument('-t', '--type', type=str, default="f",
-                        choices=["z", "f", "f-cot"])
+    parser.add_argument('-t', '--type', type=str, default="z-cot",
+                        choices=["z", "z-cot", "f", "f-cot"])
     parser.add_argument('-sn', '--samples_num', type=int, default=2000)
     parser.add_argument('-vn', '--voting_num', type=int, default=1)
     parser.add_argument('-lp', '--length_penalty', type=float, default=0.99)
     parser.add_argument('-tmp', '--temperature', type=float, default=0.01)
-    parser.add_argument('-mt', '--max_tokens', type=int, default=10)
+    parser.add_argument('-mt', '--max_tokens', type=int, default=128)
     parser.add_argument('-st', '--stop_token', type=list, default=["Q:"])
     args = parser.parse_args()
 
     sweep_configuration = {
         'method': 'random',
-        'name': 'CommonsenseQA (zero-shot)',
+        'name': 'GSM8K (zero-shot CoT)',
         'metric': {'goal': 'maximize', 'name': 'accuracy'},
         'parameters':
         {
